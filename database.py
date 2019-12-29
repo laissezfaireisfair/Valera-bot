ef5b6d44c55e0f12269b1464fa24f286622a0152
Andrey Golubev <laissezfaireisfair@protonmail.com>
2019-12-29 18:31:12 +0700

    Add creator of entry to database handler

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ class PeopleDatabase:
         self.connect = sqlite3.connect("PeopleDatabase.db")
         self.cursor = self.connect.cursor()
         self.cursor.execute("""CREATE TABLE IF NOT EXISTS people
-                            (name text, surename text, birthDay int,
+                            (creator text, name text, surename text, birthDay int,
                             birthMonth int, birthYear int, gender text)""")
         self.connect.commit()
 
@@ -13,15 +13,18 @@ class PeopleDatabase:
         self.connect.close()
 
     def add_entry(self, entry):
-        self.cursor.execute('INSERT INTO people VALUES (?,?,?,?,?,?)', entry)
+        self.cursor.execute('INSERT INTO people VALUES (?,?,?,?,?,?,?)', entry)
         self.connect.commit()
 
     def add_entry_list(self, entries):
-        self.cursor.executemany('INSERT INTO people VALUES (?,?,?,?,?,?)', entries)
+        self.cursor.executemany('INSERT INTO people VALUES (?,?,?,?,?,?,?)', entries)
         self.connect.commit()
 
     def get_entry(self, id):
         return [i for i in self.cursor.execute('SELECT * FROM people WHERE rowid=?', (id,))][0]
+
+    def get_created_entries(self, creator):
+        return self.cursor.execute('SELECT * FROM people WHERE creator=?', (creator,))
 
     def get_all_entries(self):
         return self.cursor.execute('SELECT rowid, * FROM people ORDER BY surename')
